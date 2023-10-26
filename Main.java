4a535f11252a8df898dc9598d3e37eb05b0938aa
Andrey <143511905+Iziumich@users.noreply.github.com>
2023-10-26 18:26:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1, 1);
-        int c = 0;
-        try {
-            c = calc.devide.apply(a, b);
-        } catch (ArithmeticException e) {
-            System.out.println("Делить на ноль нельзя");
-        }
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b);
         calc.println.accept(c);
     }
 }
