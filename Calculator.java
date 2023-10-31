abe825299e1176eae9120301503dd776cc9678dd
Andrey <143511905+Iziumich@users.noreply.github.com>
2023-10-31 11:26:59 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -6,11 +6,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) ->
-    {
-        if (y == 0) return 0;
-        else return x / y;
-    };
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x/y : 0;
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
